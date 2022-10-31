3e1072ad7d2604cac7d7029f182d1b643cb26d3a
JoonaTeuho <joona.teuho@gmail.com>
2022-10-31 19:11:12 +0200

    Added UUID

--- consumer/index.js
+++ consumer/index.js
@@ -1,18 +1,41 @@
 const Kafka = require('node-rdkafka')
+const { v4: uuidv4 } = require('uuid');
 
 console.log("*** Consumer starts... ***")
 
-const consumer = Kafka.KafkaConsumer({
-    'group.id':'kafka',
-    'metadata.broker.list':'localhost:9092'
-}, {})
+const taskConsumer = Kafka.KafkaConsumer({
+    'group.id': 'taskConsumers',
+    'metadata.broker.list': 'localhost:9092'
+}, {});
 
-consumer.connect()
+taskConsumer.connect()
 
-consumer.on('ready', () => {
+const answerStream = Kafka.Producer.createWriteStream({
+    'metadata.broker.list': 'localhost:9092'
+}, {}, { topic: 'answer' });
+
+taskConsumer.on('ready', () => {
     console.log('Consumer ready...')
-    consumer.subscribe(['task'])
-    consumer.consume()
+    taskConsumer.subscribe(['task'])
+    taskConsumer.consume()
 }).on('data', (data) => {
-    console.log(`received message: ${data.value}`)
+
+    const obj = JSON.parse(data.value)
+    let answer = `Calculation ${obj.o1}+${obj.o2}=${obj.o3} is`
+    if (obj.o1 + obj.o2 === obj.o3) {
+        answer += " true.";
+    } else {
+        answer += " FALSE!";
+    }
+
+    console.log(answer)
+
+    const success =
+        answerStream.write(Buffer.from(answer));
+
+    if (success) {
+        console.log(`Answer ${obj.id} successfully to stream`);
+    } else {
+        console.log('Problem writing to answer stream..');
+    }
 })
--- producer/index.js
+++ producer/index.js
@@ -1,30 +1,52 @@
 const Kafka = require('node-rdkafka')
+const { v4: uuidv4 } = require('uuid');
+
 console.log("*** Producer starts... ***")
 
-const stream = Kafka.Producer.createWriteStream({
-    'metadata.broker.list':'localhost:9092'
-}, {}, {topic: 'task'})
+const answerConsumer = Kafka.KafkaConsumer({
+    'group.id': 'answerConsumers',
+    'metadata.broker.list': 'localhost:9092'
+}, {});
+
+answerConsumer.connect();
+
+answerConsumer.on('ready', () => {
+    console.log('consumer ready...');
+    answerConsumer.subscribe(['answer']);
+    answerConsumer.consume();
+}).on('data', (data) => {
+    console.log(`received message: ${data.value}`);
+});
+
+const taskStream = Kafka.Producer.createWriteStream({
+    'metadata.broker.list': 'localhost:9092'
+}, {}, { topic: 'task' });   
 
 function randomizeIntegerBetween(from, to) {
-    return (Math.floor(Math.random() * (to-from+1))) + from
+    return (Math.floor(Math.random() * (to - from + 1))) + from
 }
 
-function queueMessage() {
-    const o1 = randomizeIntegerBetween(1,2)
-    const o2 = randomizeIntegerBetween(1,2)
-    const o3 = randomizeIntegerBetween(1,2)
+ function queueMessage() {
+    const o1 = randomizeIntegerBetween(1, 2)
+    const o2 = randomizeIntegerBetween(1, 2)
+    const o3 = randomizeIntegerBetween(1, 2)
+    const id = uuidv4().substring(30)
 
-    const success = stream.write(Buffer.from(
-        `Is this correct ${o1} + ${o2} = ${o3}?`
+    let obj = { o1, o2, o3, id };
+    let objJSON = JSON.stringify(obj);
+
+    const success = taskStream.write(Buffer.from(
+        `{"o1":${o1}, "o2":${o2}, "o3":${o3}, "id":"${id}"}`
     ))
 
-    if(success) {
-        console.log('message succesfully to stream')
-    }else {
+    if (success) {
+        console.log(`Task ${id} succesfully to stream`)
+    } else {
         console.log('Problem writing to stream..')
     }
 }
 
 setInterval( () => {
-    queueMessage()
-}, 2500)
+    queueMessage();
+}, 2500);
+    
